b39bc1233a04eb38429a1c96626f1faf3967514c
nagacharan <nagacharan4286@gmail.com>
2025-03-20 11:58:01 +0530

    executing shell script

---
+++ anscii-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install cowsay -y
+
+cowsay -f dragon "Run for the cover, i am a dragon ....RAWR" >> dragon.txt
+
+grep -i "dragon" dragon.txt
+
+cat dragon.txt
+
+ls -lrta
